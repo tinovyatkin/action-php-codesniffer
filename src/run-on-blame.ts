e53e6b5a17fde7296ec17338c6380bc32d4964b9
Nabeel Ahmed <thisisnabeelahmed@gmail.com>
2021-01-05 18:53:24 +0500

    Logging options

--- src/run-on-blame.ts
+++ src/run-on-blame.ts
@@ -35,7 +35,7 @@ export async function runOnBlame(files: string[]): Promise<void> {
       ['--no-pager', 'log', '--format=%ae', `${github.context.sha}^!`],
       { encoding: 'utf8', windowsHide: true, timeout: 5000 }
     ).trim();
-    console.log('PR author email: %s', authorEmail);
+    console.log('PR author email: %s', 'test');
     console.log('Standard: %s', standard);
     console.log('Options: %s', options);
 
